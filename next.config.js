a25dbaa8e1b7ab020f2124b75e2b6f4d6abf2878
SUN <ksenia.zielimska@gmail.com>
2023-02-17 21:12:58 +0900

    change next config file

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     // Enables the styled-components SWC transform
     styledComponents: true
   },
-  staticPageGenerationTimeout: 100,
+  staticPageGenerationTimeout: 1000 * 60,
 }
 
 module.exports = nextConfig
